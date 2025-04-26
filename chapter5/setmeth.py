4c18d64fbe4178806ae5db2ea73b9cc2fc47f99f
Bashibav <bhatttarun1999@gmail.com>
2025-04-26 21:53:41 +0545

    6th day python

---
+++ chapter5/setmeth.py
@@ -0,0 +1,9 @@
+set={55,2,45,98,"toya"}
+print(set)
+print(type(set))
+
+#method
+#add method
+set.add(52)
+print(set)
+#cannot replace elements of sets but can remove and add new one
---
+++ chapter5/sets.py
@@ -0,0 +1,4 @@
+disk={} #empty dictionary
+
+set={1,5,8,56,75,22} #creat dictionaty
+es=set() # creates empty set and set does not contains repeatation elements
